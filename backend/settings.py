0a80de0841aef1bf2c9d5a46cf06e9f313c0ea56
Fernando Ribeiro Aguilar <fernando.aguilar@hotmail.com.br>
2021-10-19 21:05:09 -0500

    Add initial models and respective unit tests

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'otc_twitter'
 ]
 
 MIDDLEWARE = [
---
+++ backend/tests/tests.py
@@ -0,0 +1,25 @@
+import unittest
+from django.test import TestCase
+
+
+class TestBasic2(unittest.TestCase):
+    "Show setup and teardown"
+
+    def setUp(self):
+        self.a = 1
+
+    def tearDown(self):
+        del self.a
+
+    def test_basic1(self):
+        "Basic with setup"
+
+        self.assertNotEqual(self.a, 2)
+
+    def test_basic2(self):
+        "Basic2 with setup"
+        assert self.a != 2
+
+    def test_fail(self):
+        "This test should't fail"
+        assert self.a == 1
