05024b1f8e1f4a6aa669396947f26f2103dc68d8
Fernando Ribeiro Aguilar <fernando.aguilar@hotmail.com.br>
2021-10-23 20:17:22 -0500

    add pseudocode for the bot

--- omni/main.py
+++ omni/main.py
@@ -1,14 +1,47 @@
 from datetime import datetime
 from twitter.tweet_scraper import search_from_specific_user
+import reticker
 
+extractor = reticker.TickerExtractor()
 
-def get_tweets():
-    arr_of_vip_accs = [
-        "fernand0aguilar"
-    ]
+arr_of_vip_accs = [
+    "mn_goat_trader"
+]
+
+def get_tickers():
+    arr_with_all_results = []
     for user in arr_of_vip_accs:
-        search_results = search_from_specific_user(user)
+        search_results = extractor.extract(search_from_specific_user(user))
         print('search results', search_results)
+        arr_with_all_results.push(search_results)
+    # here -> #duplicates are the counts, loop through the results and find dict where the key is the tickername and the count is how many times we find it in the arr
+    # after that we sort the dict by the counts and so the ones that habe highest counts goes to the top 
+    # finally we send this to another function called prepare_email(ticker_scores):
+
+def prepare_email ():
+    # top otc stocks of the last 24 hours
+    
+    # 1. $TGGI [stocktwits] [yahoo] [stockcharts] [otcmarkets] [twitter]
+    # 2. do it for the scored tickers of the day
+    
+    # get the guide to succinct stock market success on gumroad
+    # presale item <link> | # get your referral link
+    
+    return
+
+def get_emails():
+    # fetch product subscribers from gumroad
+    # get subscribed emails
+    return
+
+def send_mail():
+    # get_emails()    
+    # get tickers, prepare email, loop over emails, send them in a batch
+    # sg.send()
+    
+    return
+
+
 
 def cronjob():
     """
@@ -17,3 +50,5 @@ def cronjob():
     """
     print("Cron job is running")
     print("Tick! The time is: %s" % datetime.now())
+    send_mail()
+
--- omni/twitter/tweet_scraper.py
+++ omni/twitter/tweet_scraper.py
@@ -1,4 +1,6 @@
-from snscrape.modules import twitter
+
+# Imports
+import snscrape.modules.twitter as sntwitter
 
 def search_from_specific_user(user):
     maxTweets = 100
@@ -6,22 +8,26 @@ def search_from_specific_user(user):
     # Creating list to append tweet data to
     tweets_list1 = []
     print('here')
-    for i, tweet in enumerate(twitter.TwitterSearchScraper('from:{}'.format(user)).get_items()):
+    # Using TwitterSearchScraper to scrape data
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper('from:{}'.format(user)).get_items()):
         if i > maxTweets:
             break
         without_mentions = tweet.content.split("@", 1)[0]
         without_links = without_mentions.split("https://", 1)[0]
         tweets_list1.append([without_links])
 
+    # Creating a dataframe from the tweets list above
     return tweets_list1
     # TODO ->     https://stackoverflow.com/a/41786472
 
 def search_from_search_query(search_query):
+    
     tweets_list2 = []
-    for i, tweet in enumerate(twitter.TwitterSearchScraper(search_query).get_items()):
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(search_query).get_items()):
         if i > 500:
             break
         tweets_list2.append(
             [tweet.date, tweet.id, tweet.content, tweet.user.username])
 
+    # Creating a dataframe from the tweets list above
     # TODO 2 -> https://stackoverflow.com/a/66310138
